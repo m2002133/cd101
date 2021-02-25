d64e8644b21c09fb568ea5f7d79fcfd493bf264b
m2002133 <m2002133@users.noreply.github.com>
2021-02-25 16:08:12 +0300

    hw2

---
+++ hw2.py
@@ -0,0 +1,73 @@
+#lst = [1,2,,44,68,33,25,97]
+# for x in lst:
+#    print(x)
+
+#for x in enumerate(lst):# выводить с индексом
+#print(idx,val)
+
+#for val in lst:
+    
+    #prinnt(val)
+
+
+# lst = [1,2,-44,68,33,-25,97]
+# for val in lst:
+#     if val 1 % 2==0:
+#         print("val is even")
+#     else:
+#         print("val is NOT even")
+
+# lst = [1,2,-44,68,33,-25,97]
+# for val in lst:
+#   if val 1 % 2 == 0 and val > = 0:
+#         print("val is even")
+#     else:
+#         print("val is NOT even")
+
+
+
+# lst=[35,46,98,4,87,658,147,65,.34,987,36,47,3]
+# even=[]
+# n_even=[]
+# for x in lst: # это каждое значение
+#     if x%2==0: # x  %(показываеь что осталось после деления например 10/3=9 остаток 1)
+#         even.append(x)
+#     else:
+#         n_even.append(x)
+# print(lst)
+# print(even)
+# print(n_even)
+
+# N=input
+# names = []
+# ages = []
+
+# while True:
+#     user_data = input("enter your name").split (",")
+#     names.append(user_data[0], int(user_data[1]))
+#     ages.append(int(used_data[1]))
+#     age_sum = age_sum + int(user_data[1])
+
+#     status = input ("one more? press Y if contine. Otherwise press N:")
+#     if status == "N":
+#         break
+
+# for idx, val in enumerate(names):
+#     print(f"User{idx}: {val}, {ages[idx]}")
+
+# age_sum = 0
+# for age in ages:
+#     avg_sum = age_sum / len(ages)
+# print(avg_sum)
+
+# data = {"pablo":33, "Mark": 23,}
+# print(data["pablo"])
+
+# data = {"pablo":33, "Mark": 23,}
+# for key, val in data.items():
+#     print["key, val"]
+
+    def first_foo (x, y):
+        a= x + y
+        print(a)
+first_foo(39, 88)
